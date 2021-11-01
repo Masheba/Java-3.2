3fb51f7f3215bfd703f31892b1c0616eaa2d4c58
Masheba <nikolaiageev1991@gmail.com>
2021-11-01 14:38:31 +0300

    V2

--- src/main/java/ru/netology/domian/CommentsInfo.java
+++ src/main/java/ru/netology/domian/CommentsInfo.java
@@ -2,7 +2,6 @@ package ru.netology.domian;
 
 public class CommentsInfo {
     private int commentsCounter;
-    private boolean canPost;
     private boolean canComment;
     private boolean canOpen;
     private boolean canClose;
